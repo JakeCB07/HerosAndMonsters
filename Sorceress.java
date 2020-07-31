5c75677ad2d83cf82fea6f22a4393dbcecaed94f
colmendean123 <68941235+colmendean123@users.noreply.github.com>
2020-07-30 21:41:09 -0700

    Add files via upload

--- Sorceress.java
+++ Sorceress.java
@@ -1,52 +1,27 @@
 
-
-/**
- * Title:
- * Description:
- * Copyright:    Copyright (c) 2001
- * Company:
- * @author
- * @version 1.0
- */
-
-
-
-public class Sorceress extends Hero
-{
+public class Sorceress extends Hero {
 	public final int MIN_ADD = 25;
 	public final int MAX_ADD = 50;
 
-//-----------------------------------------------------------------
-    public Sorceress()
+
+    public Sorceress(String name, AttackBehavior attack)
 	{
-		super("Sorceress", 75, 5, .7, 25, 50, .3);
+		super("Sorceress", 75, 5, .7, 25, 50, 25, .3, attack);
 
 
-    }//end constructor
-
-//-----------------------------------------------------------------
+    }
 	public void increaseHitPoints()
     {
 	    int hPoints;
 
 		hPoints = (int)(Math.random() * (MAX_ADD - MIN_ADD + 1)) + MIN_ADD;
 		addHitPoints(hPoints);
-		System.out.println(name + " added [" + hPoints + "] points.\n"
+		System.out.println(getName() + " added [" + hPoints + "] points.\n"
 							+ "Total hit points remaining are: "
-							+ hitPoints);
+							+ getHitPoints());
 		 System.out.println();
 
-    }//end increaseHitPoints method
-
-//-----------------------------------------------------------------
-	public void attack(DungeonCharacter opponent)
-	{
-		System.out.println(name + " casts a spell of fireball at " +
-							opponent.getName() + ":");
-		super.attack(opponent);
-	}//end override of attack method
-
-//-----------------------------------------------------------------
+    }
     public void battleChoices(DungeonCharacter opponent)
 	{
 		super.battleChoices(opponent);
@@ -57,7 +32,7 @@ public class Sorceress extends Hero
 		    System.out.println("1. Attack Opponent");
 		    System.out.println("2. Increase Hit Points");
 		    System.out.print("Choose an option: ");
-		    choice = Keyboard.readInt();
+		    choice = getKeyBoard().nextInt();
 
 		    switch (choice)
 		    {
@@ -67,14 +42,13 @@ public class Sorceress extends Hero
 			        break;
 			    default:
 			        System.out.println("invalid choice!");
-		    }//end switch
+		    }
 
-			numTurns--;
-		    if (numTurns > 0)
-			    System.out.println("Number of turns remaining is: " + numTurns);
+			killTurn();
+		    if (getNumTurns() > 0)
+			    System.out.println("Number of turns remaining is: " + getNumTurns());
 
-		} while(numTurns > 0 && hitPoints > 0 && opponent.getHitPoints() > 0);
+		} while(getNumTurns() > 0 && getHitPoints() > 0 && opponent.getHitPoints() > 0);
 
-    }//end overridden method
-
-}//end class
+    }
+}
