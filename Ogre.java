701b6948dbcf73d1ceae0c3f10ac33a52a269cff
colmendean123 <68941235+colmendean123@users.noreply.github.com>
2020-07-30 21:30:42 -0700

    Add files via upload

--- Ogre.java
+++ Ogre.java
@@ -1,31 +1,11 @@
 
-
-/**
- * Title:
- * Description:
- * Copyright:    Copyright (c) 2001
- * Company:
- * @author
- * @version 1.0
- */
-
-public class Ogre extends Monster
-{
-
-    public Ogre()
+public class Ogre extends Monster {
+	public Ogre(String name, AttackBehavior attack)
 	{
-		super("Oscar the Ogre", 200, 2, .6, .1, 30, 50, 30, 50);
+		super(name, 200, 2, .6, .1, 30, 50, 30, 50, attack);
 
 
-    }//end constructor
-
-	public void attack(DungeonCharacter opponent)
-	{
-		System.out.println(name + " slowly swings a club toward's " +
-							opponent.getName() + ":");
-		super.attack(opponent);
-
-	}//end override of attack
+    }
 
 
-}//end Monster class
+}
