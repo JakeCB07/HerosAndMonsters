397c2828fb655b595c9cbfb4a3da9f010db518c4
colmendean123 <68941235+colmendean123@users.noreply.github.com>
2020-07-30 21:45:12 -0700

    Add files via upload

--- Thief.java
+++ Thief.java
@@ -1,75 +1,63 @@
 
-
-/**
- * Title:
- * Description:
- * Copyright:    Copyright (c) 2001
- * Company:
- * @author
- * @version 1.0
- */
-
-public class Thief extends Hero
-{
-
-    public Thief()
-	{
-		super("Thief", 75, 6, .8, 20, 40, .5);
-
-
-
-    }//end constructor
-
-	public void surpriseAttack(DungeonCharacter opponent)
-	{
-		double surprise = Math.random();
-		if (surprise <= .4)
+public class Thief extends Hero {
+	 public Thief(String name, AttackBehavior attack)
 		{
-			System.out.println("Surprise attack was successful!\n" +
-								name + " gets an additional turn.");
-			numTurns++;
-			attack(opponent);
-		}//end surprise
-		else if (surprise >= .9)
+			super(name, 75, 6, .8, 20, 40, 5, .5, attack);
+
+
+
+	    }
+
+		public void surpriseAttack(DungeonCharacter opponent)
 		{
-			System.out.println("Uh oh! " + opponent.getName() + " saw you and" +
-								" blocked your attack!");
+			double surprise = Math.random();
+			if (surprise <= .4)
+			{
+				System.out.println("Surprise attack was successful!\n" +
+									getName() + " gets an additional turn.");
+				addTurns();
+				attack(opponent);
+			}
+			else if (surprise >= .9)
+			{
+				System.out.println("Uh oh! " + opponent.getName() + " saw you and" +
+									" blocked your attack!");
+			}
+			else
+			    attack(opponent);
+
+
 		}
-		else
-		    attack(opponent);
 
 
-	}//end surpriseAttack method
-
-
-    public void battleChoices(DungeonCharacter opponent)
-	{
-		super.battleChoices(opponent);
-		int choice;
-
-
-		do
+	    public void battleChoices(DungeonCharacter opponent)
 		{
-		    System.out.println("1. Attack Opponent");
-		    System.out.println("2. Surprise Attack");
-		    System.out.print("Choose an option: ");
-		    choice = Keyboard.readInt();
+			super.battleChoices(opponent);
+			int choice;
 
-		    switch (choice)
-		    {
-			    case 1: attack(opponent);
-			        break;
-			    case 2: surpriseAttack(opponent);
-			        break;
-			    default:
-			        System.out.println("invalid choice!");
-		    }//end switch
 
-			numTurns--;
-			if (numTurns > 0)
-			    System.out.println("Number of turns remaining is: " + numTurns);
+			do
+			{
+			    System.out.println("1. Attack Opponent");
+			    System.out.println("2. Surprise Attack");
+			    System.out.print("Choose an option: ");
+			    choice = getKeyBoard().nextInt();
 
-		} while(numTurns > 0);
+			    switch (choice)
+			    {
+				    case 1: attack(opponent);
+				        break;
+				    case 2: surpriseAttack(opponent);
+				        break;
+				    default:
+				        System.out.println("invalid choice!");
+			    }//end switch
 
-    }
+				killTurn();
+				if (getNumTurns() > 0)
+				    System.out.println("Number of turns remaining is: " + getNumTurns());
+
+			} while(getNumTurns() > 0);
+
+	    }
 }
--- Warrior.java
+++ Warrior.java
@@ -1,85 +1,73 @@
 
-
-/**
- * Title:
- * Description:
- * Copyright:    Copyright (c) 2001
- * Company:
- * @author
- * @version 1.0
- */
-
-
-
-
-public class Warrior extends Hero
-{
-
-    public Warrior()
-	{
-
-		super("Warrior", 125, 4, .8, 35, 60, .2);
-
-
-    }//end constructor
-
-
-	public void crushingBlow(DungeonCharacter opponent)
-	{
-		if (Math.random() <= .4)
+public class Warrior extends Hero {
+	
+	  public Warrior(String name, AttackBehavior attack)
 		{
-			int blowPoints = (int)(Math.random() * 76) + 100;
-			System.out.println(name + " lands a CRUSHING BLOW for " + blowPoints
-								+ " damage!");
-			opponent.subtractHitPoints(blowPoints);
-		}//end blow succeeded
-		else
+
+		  
+		  
+			super(name, 125, 4, .8, 35, 60, 5, .2, attack);
+
+
+	    }
+	  
+	  
+
+		public void crushingBlow(DungeonCharacter opponent)
 		{
-			System.out.println(name + " failed to land a crushing blow");
-			System.out.println();
-		}//blow failed
+			if (Math.random() <= .4)
+			{
+				int blowPoints = (int)(Math.random() * 76) + 100;
+				System.out.println(this.getName() + " lands a CRUSHING BLOW for " + blowPoints
+									+ " damage!");
+				opponent.subtractHitPoints(blowPoints);
+			}
+			else
+			{
+				System.out.println(getName() + " failed to land a crushing blow");
+				System.out.println();
+			}
 
-	}//end crushingBlow method
+		}
 
-	public void attack(DungeonCharacter opponent)
-	{
-		System.out.println(name + " swings a mighty sword at " +
-							opponent.getName() + ":");
-		super.attack(opponent);
-	}//end override of attack method
-
-
-
-
-    public void battleChoices(DungeonCharacter opponent)
-	{
-		int choice;
-
-		super.battleChoices(opponent);
-
-		do
+		public void attack(DungeonCharacter opponent)
 		{
-		    System.out.println("1. Attack Opponent");
-		    System.out.println("2. Crushing Blow on Opponent");
-		    System.out.print("Choose an option: ");
-		    choice = Keyboard.readInt();
+			System.out.println(getName() + " swings a mighty sword at " +
+								opponent.getName() + ":");
+			super.attack(opponent);
+		}
 
-		    switch (choice)
-		    {
-			    case 1: attack(opponent);
-			        break;
-			    case 2: crushingBlow(opponent);
-			        break;
-			    default:
-			        System.out.println("invalid choice!");
-		    }//end switch
 
-			numTurns--;
-			if (numTurns > 0)
-			    System.out.println("Number of turns remaining is: " + numTurns);
 
-		} while(numTurns > 0);
 
-    }//end battleChoices method
+	    public void battleChoices(DungeonCharacter opponent)
+		{
+			int choice;
 
-}//end Hero class
+			super.battleChoices(opponent);
+
+			do
+			{
+			    System.out.println("1. Attack Opponent");
+			    System.out.println("2. Crushing Blow on Opponent");
+			    System.out.print("Choose an option: ");
+			    choice = getKeyBoard().nextInt();
+
+			    switch (choice)
+			    {
+				    case 1: attack(opponent);
+				        break;
+				    case 2: crushingBlow(opponent);
+				        break;
+				    default:
+				        System.out.println("invalid choice!");
+			    }
+
+				killTurn();
+				if (getNumTurns() > 0)
+				    System.out.println("Number of turns remaining is: " + getNumTurns());
+
+			} while(getNumTurns() > 0);
+
+	    }
+}
