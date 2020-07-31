6fcca6f92fb998df481ffc0c7a75e9b5b8f1fd4c
MarkCapka <61602557+MarkCapka@users.noreply.github.com>
2020-07-31 12:49:32 -0700

    Update MonsterFactory.java
    I may need to make some adjustments to the protected abstract classes, but right now it looks good.

--- HerosAndMonsters-Capkas_MainBranch/HerosAndMonsters-Capkas_MainBranch/MonsterFactory.java
+++ HerosAndMonsters-Capkas_MainBranch/HerosAndMonsters-Capkas_MainBranch/MonsterFactory.java
@@ -1,22 +1,43 @@
-    public class MonsterFactory 
+
+
+public abstract class MonsterFactory implements CharacterFactory<Monster>
     {
     
-        public static Ogre createOgre(Monster theMonster, int chanceToHeal, int minHeal, int maxHeal)
-        {
-            
-            
-    		return  new Ogre(theMonster, chanceToHeal, minHeal, maxHeal);
-        }
-        
-        public static Skeleton createSkeleton(Monster theMonster, int chanceToHeal, int minHeal, int maxHeal)
-        {
-    		return new Skeleton(theMonster, chanceToHeal, minHeal, maxHeal);
-        }
-        
-        public static Gremlin createGremlin(Monster theMonster, int chanceToHeal, int minHeal, int maxHeal)
-        {
-    		return new Gremlin(theMonster, chanceToHeal, minHeal, maxHeal);
-        }
-        
+
+
+	@Override
+	public Monster create(String name)
+	{
+
+		int choice;
+
+		choice = (int)(Math.random() * 3) + 1;
+
+		switch(choice)
+		{
+			case 1: return createOgre(Ogre.getName(), Ogre.getHitPoints(), Ogre.getAttackSpeed(), Ogre.getAttackBehavior());
+
+			case 2: return createGremlin(Gremlin.getName(), Gremlin.getHitPoints(), Gremlin.getAttackSpeed(), Gremlin.getAttackBehavior());
+
+			case 3: return createSkeleton(Skeleton.getName(), Skeleton.getHitPoints(), Skeleton.getAttackSpeed(), Skeleton.getAttackBehavior());
+
+			default: System.out.println("invalid choice, returning Skeleton");
+				     return createSkeleton(Skeleton.getName(), Skeleton.getHitPoints(), Skeleton.getAttackSpeed(), Skeleton.getAttackBehavior());
+		}//end switch
+	}
+
+	protected abstract Monster createOgre(String name, int hitPoints, int attackSpeed,
+		AttackBehavior attackBehavior);
+
+	protected abstract Monster createGremlin(String name, int hitPoints, int attackSpeed,
+		AttackBehavior attackBehavior);
+
+	protected abstract Monster createSkeleton(String name, int hitPoints, int attackSpeed,
+		AttackBehavior attackBehavior);
+
+	
+	
+	
+	        
     
-    }
+}
