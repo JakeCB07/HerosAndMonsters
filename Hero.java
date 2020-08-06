0ac65b77c64d33a4cc572a848ae3faa0776c08b9
colmendean123 <68941235+colmendean123@users.noreply.github.com>
2020-08-05 22:20:00 -0700

    Add files via upload

--- Hero.java
+++ Hero.java
@@ -1,15 +1,28 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Hero extends DungeonCharacter {
     
-
+    
 	private int numTurns;
-	
+	private Location point;
+	protected double chanceToBlock;
+	protected ArrayList<Item> inventory;
+    protected String name;
+	protected Scanner playerInput = new Scanner(System.in);
 	public Hero(String name, int hitPoints, int attackSpeed, double chanceToHit, int damageMin, int damageMax,
 			 int numTurns, double chanceToBlock, AttackBehavior attack) {
 		super(name, hitPoints, attackSpeed, chanceToHit, damageMin, damageMax, chanceToBlock, numTurns, attack);
+		
 	}
 	
-
+	public void setPoint(int x, int y) {
+		point=new Location(x,y);
+	}
+     public Location getPoint() {
+    	 return this.point;
+     }
+ 
 	public void subtractHitPoints(DungeonCharacter opponent) {
 		opponent.getAttackBehavior().attack(opponent, this);
 	}
@@ -17,8 +30,8 @@ public class Hero extends DungeonCharacter {
 	{
 	    numTurns = getAttackSpeed()/opponent.getAttackSpeed();
 
-		if (getNumTurns() < 0)
-			setTurns(1);
+		if (getNumTurns() <= 0)
+			addTurns();
 
 		System.out.println("Number of turns this round is: " + getNumTurns());
 
