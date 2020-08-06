4186c21ad167de39648f72c72e85e603b9c1b350
colmendean123 <68941235+colmendean123@users.noreply.github.com>
2020-08-05 22:08:31 -0700

    New movement

--- Dungeon.java
+++ Dungeon.java
@@ -1,20 +1,41 @@
 
 public class Dungeon {
-	 public static void main(String[] args)
-		{
-		 GameManager gameManager = new GameManager();
-			Hero theHero;
-			Monster theMonster;
-
-			do
-			{
-			    theHero = gameManager.chooseHero();
-			    theMonster = gameManager.generateMonster();
-				gameManager.battle(theHero, theMonster);
-
-			} while (gameManager.playAgain());
-
-	    }
-
-
+	private Room[][] room;
+	private String wallH=" *-*";
+    private String wallV="*| |";
+	
+	public Dungeon(int rows, int columns) {
+		rows=5;
+		columns=5;
+		room=new Room[rows][columns];
 	}
+	public void makeMap() {
+		
+		 for (int r = 0; r < room.length; r++) {
+	            for (int c = 0; c < room[r].length; c++) {
+	            	
+	                room[r][c] = new Room();
+	                room[r][c].setRoomNumberX(r);
+	                room[r][c].setRoomNumberY(c);
+	               
+	                
+	            }
+		 }
+	}
+	public void printLocation(Hero hero) {
+		System.out.println("Our hero is in: " +room[hero.getPoint().getX()][hero.getPoint().getY()].printRoomNumber());
+		System.out.println(room[hero.getPoint().getX()][hero.getPoint().getY()]);
+	}
+	public void lookAtMap() {
+		for(int i=0; i<room.length; i++) {
+		
+			for(int x=0; x<room[i].length;x++) {
+                 
+				 
+		}
+		
+	}
+	}
+
+	
+}
---
+++ Location.java
@@ -0,0 +1,54 @@
+
+public class Location {
+private int x, y;
+
+public Location(int x, int y) {
+	this.x=x;
+	this.y=y;
+}
+public int getX() {
+	return this.x;
+}
+public int getY() {
+	return this.y;
+}
+public void setX(int x) {
+	this.x=x;
+}
+public void setY(int y) {
+	this.y=y;
+	
+}
+public void moveNorth() {
+	if(y !=0) {
+	y-=1;
+	}
+	else {
+		System.out.println("I'm stuck at the top of the map");
+	}
+}
+public void moveSouth() {
+	if(y !=4) {
+	y+=1;
+	}
+	else {
+		System.out.println("I'm at the bottom of the map");
+	}
+}
+public void moveEast() {
+	if(x !=4) {
+	x+=1;
+	}
+	else {
+		System.out.println("I'm as far east as I can go");
+	}
+}
+public void moveWest() {
+	if(x !=0) {
+	x-=1;
+	}
+	else {
+		System.out.println("I'm as far west as I can go");
+	}
+}
+}
